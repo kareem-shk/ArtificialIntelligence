75a26d14e103dc124295b8f1767ff0850678230b
kareem shaik <kareem.shk21@gmail.com>
2018-12-23 09:36:42 +0530

    Added pretrained net

--- Projects/FacialKeyPointsDetection/models.py
+++ Projects/FacialKeyPointsDetection/models.py
@@ -95,3 +95,38 @@ class Net(nn.Module):
 
         #y = self.fc2(y)           
         return y
+
+class TransferNet(nn.Module):
+
+    def __init__(self, features, num_classes):
+        super(TransferNet, self).__init__()
+        
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        self.features = features
+        self.features_ = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(), 
+            #nn.MaxPool2d(kernel_size=3, stride=2),
+            #nn.Conv2d(in_channels=1024, out_channels=2048, kernel_size=3, stride=1, padding=0),
+            #nn.ReLU(), 
+        )
+        self.fc1 = nn.Linear(512*7*7, 2048)
+        self.fc2 = nn.Linear(2048, num_classes)
+       
+    def forward(self, x):
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+        ## x = self.pool(F.relu(self.conv1(x)))   
+        # a modified x, having gone through all the layers of your model, should be returned
+        y = self.features(x)
+        #y = self.features_(y)
+        y = y.view(y.size(0), -1)
+        y = F.relu(self.fc1(y))   
+        y = self.fc2(y)   
+        return y
